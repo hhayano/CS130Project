a009484c5c9fcfe7a38379f0fb9a8596cec62ebb
leoball <liyang.ucla@outlook.com>
2018-11-14 22:52:28 -0800

    add features of user and database data reader

--- project/project/AppDelegate.swift
+++ project/project/AppDelegate.swift
@@ -18,10 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        for i in 1...10{
-            Database_.updateUser(User(i,"hello",i))
+        for i in 1...20{
+            User(i,"hello",i).submitUser()
         }
         
+//
         return true
     }
 
--- project/project/Database.swift
+++ project/project/Database.swift
@@ -7,32 +7,32 @@ class Database_ {
     
     static var ref:DatabaseReference! = Database.database().reference()
     
-    static func updateUser(_ user:User) -> Void {
-        let request :[String: Any?] = ["name": user.name,
-                       "weeklyArrowCount": user.weeklyArrowCount,
-//            "prevScoringRounds" : user.prevScoringRounds,
-//            "SavedPresets" : user.SavedPresets,
-//            "SavedJournalEntries" : user.SavedJournalEntries
-        ]
-        
+    static func addOrUpdateUser(_ user:User) -> Void {
+        let request :[String: Any?] = user.updaterequest()
         ref.child("users").child(String(user.uid)).setValue(request)
-        
+        }
+    
+    static func deleteUser(_ user:User) -> Void {
+        ref.child("users").child(String(user.uid)).removeValue()
     }
+    
      
-     func getUser() -> User {
+      static func getUser(_ user:User) -> Void{
        // to do: get all the info of User and return a User object?
-       let userID = Auth.auth().currentUser?.uid
-       ref.child("users").child(userID!).observeSingleEvent(of: .value, with: { (snapshot) in
+      ref.child("users").child(String(user.uid)).observeSingleEvent(of: .value, with: { (snapshot) in
        // Get user value
-       let value = snapshot.value as? NSDictionary
-       let username = value?["username"] as? String ?? ""
-       let user = User(username: username)
-    
-       // ...
-     }) { (error) in
+       let value = snapshot.value as? [String: Any]
+       let username = value?["name"] as? String ?? ""
+       user.weeklyArrowCount = value?["weeklyArrowCount"] as? Int ?? 0
+       user.name = username
+       print(username)
+        // ...
+        }) { (error) in
      print(error.localizedDescription)
+            
      }
-    
-     }
+        
+        
+}
 }
 
