43ade761898ac4c4142a34f3d28332848e7845b0
leoball <liyang.ucla@outlook.com>
2018-11-14 16:10:44 -0800

    change database implementations

--- project/project/Database.swift
+++ project/project/Database.swift
@@ -1,44 +1,37 @@
 
-
-
-
-
+import Foundation
+import Firebase
+import FirebaseDatabase
 class Database {
-        // create a reference to firebase
-        var ref: DatabaseReference!
-        ref = Database.database().reference()
-        func updateUser(user:User) -> Void {
-          let request = [
-            "name": user.name,
-            "weeklyArrowCount": user.weeklyArrowCount
-            ]
-
-          self.ref.child("users").child(user.uid).setValue(request)
-          
-        }
-        // Look up the database and get the unique User 
-        func getUser() -> User {
-          // to do: get all the info of User and return a User object?
-          let userID = Auth.auth().currentUser?.uid
-          ref.child("users").child(userID!).observeSingleEvent(of: .value, with: { (snapshot) in
-          // Get user value
-          let value = snapshot.value as? NSDictionary
-          let username = value?["username"] as? String ?? ""
-          let user = User(username: username)
-
-          // ...
-        }) { (error) in
-        print(error.localizedDescription)
-        }
-          
-        }
+    // create a reference to firebase
+    var ref: DatabaseReference!
+    let ref = Database.database().reference()
+    func updateUser(user:User) -> Void {
+        let request :[String: Any?] = ["name": user.name,
+                       "weeklyArrowCount": user.weeklyArrowCount,
+//            "prevScoringRounds" : user.prevScoringRounds,
+//            "SavedPresets" : user.SavedPresets,
+//            "SavedJournalEntries" : user.SavedJournalEntries
+        ]
+        
+        self.ref.child("users").child(user.uid).setValue(request)
+        
+    }
+    // Look up the database and get the unique User
+    // func getUser() -> User {
+    //   // to do: get all the info of User and return a User object?
+    //   let userID = Auth.auth().currentUser?.uid
+    //   ref.child("users").child(userID!).observeSingleEvent(of: .value, with: { (snapshot) in
+    //   // Get user value
+    //   let value = snapshot.value as? NSDictionary
+    //   let username = value?["username"] as? String ?? ""
+    //   let user = User(username: username)
+    
+    //   // ...
+    // }) { (error) in
+    // print(error.localizedDescription)
+    // }
+    
+    // }
 }
-
-
- var uid:Int
-    var name:String?
-    var weeklyArrowCount:Int
-    var prevScoringRounds:[ScoringRound]
-    var SavedPresets:[Preset]
-    var SavedJournalEntries:[JournalEntry]
 
--- project/project/User.swift
+++ project/project/User.swift
@@ -1,46 +1,39 @@
 //
-//  Member.swift
+//  User.swift
 //  project
 //
-//  Created by li yang on 11/13/18.
+//  Created by li yang on 11/14/18.
 //  Copyright © 2018 Chungchhay Kuoch. All rights reserved.
 //
 
 import Foundation
-
-
 class User {
     var uid:Int
     var name:String?
     var weeklyArrowCount:Int
-    var prevScoringRounds:[ScoringRound]
-    var SavedPresets:[Preset]
-    var SavedJournalEntries:[JournalEntry]
-
-    init(_ name:String, _ weeklyArrowCount: Int ) {
+//    var prevScoringRounds:[ScoringRound]
+//    var SavedPresets:[Preset]
+//    var SavedJournalEntries:[JournalEntry]
+    
+    init(_ uid:Int, _ name: String, _ weeklyArrowCount: Int) {
+        self.uid = uid
         self.name = name
         self.weeklyArrowCount = weeklyArrowCount
     }
-    func 
     
-    func addData(_ data: Preset) {
-        self.SavedPresets.append(data)
-    }
-
-    func addData(_ data: ScoringRound) {
-        self.prevScoringRounds.append(data)
-    }
-
-    func addData(_ data: JournalEntry) {
-        self.SavedJournalEntries.append(data)
-    }
-
-    func addData(_ data: JournalEntry) {
-        self.SavedJournalEntries.append(data)
-    }
-    
-
-    
-
-  
+//    func addData(_ data: Preset) {
+//        self.SavedPresets.append(data)
+//    }
+//
+//    func addData(_ data: ScoringRound) {
+//        self.prevScoringRounds.append(data)
+//    }
+//
+//    func addData(_ data: JournalEntry) {
+//        self.SavedJournalEntries.append(data)
+//    }
+//
+//    func addData(_ data: JournalEntry) {
+//        self.SavedJournalEntries.append(data)
+//    }
 }
